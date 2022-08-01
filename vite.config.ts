ca1444f2b807e2e97e89394de1682d579e514ef6
YaroslavWeb <yaroslavweb98@gmail.com>
2022-08-01 21:43:55 +0800

    update vite deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,11 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/plankton-vue/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
